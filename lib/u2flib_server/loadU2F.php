d58a4d60f39d94c8bff802e27a44da9d98f1619c
avm99963 <jocdeladria@gmail.com>
2016-01-23 12:35:09 +0100

    Fixed u2flib-server problem with the repo

--- lib/u2flib_server/loadU2F.php
+++
@@ -1,6 +0,0 @@
-<?php
-require("vendor/autoload.php");
-require("U2F.php");
-$scheme = isset($_SERVER['HTTPS']) ? "https://" : "http://";
-$u2f = new u2flib_server\U2F($scheme . $_SERVER['HTTP_HOST']);
-?>
